baed74288a88bb7a36368ca67e14a98b9ae193d2
Joeanchae9835 <dmsco9835@naver.com>
2020-10-14 14:55:37 +0900

    error

--- MyApp.java
+++ MyApp.java
@@ -9,20 +9,26 @@ import java.io.*;
 public class MyApp
 {
     public static void main(String[] args){
-        File inputFile = new File
-            ("C:\\Users\\Jo Eanchae\\Desktop\\2020 - 2학기\\프로그래밍응용2(PBL)\\연습\\Project_03 (20201014)\\sunmoon.jpg");
-        FileInputStream fis = new FileInputStream(inputFile);
-        BufferedInputStream bis = new BufferedInputStream(fis, 20);
-        
-        File outputFile = new File("C:\\Users\\Jo Eanchae\\Desktop\\2020 - 2학기\\프로그래밍응용2(PBL)\\연습\\Project_03 (20201014)\\sunmoon-copy.jpg");
-        FileOutputStream fos = new FileOutputStream(outputFile);
-        BufferedOutputStream bos = new BufferedOutputStream(fos, 20);
-        
-        int c;
-        while((c = fis.read()) != -1){
-            fos.write((byte)c);
+        try{
+            File inputFile = new File
+                ("C:\\Users\\Jo Eanchae\\Desktop\\2020 - 2학기\\프로그래밍응용2(PBL)\\연습\\Project_03 (20201014)\\sunmoon.jpg");
+            FileInputStream fis = new FileInputStream(inputFile);
+            BufferedInputStream bis = new BufferedInputStream(fis, 20);
+
+            File outputFile = new File
+                ("C:\\Users\\Jo Eanchae\\Desktop\\2020 - 2학기\\프로그래밍응용2(PBL)\\연습\\Project_03 (20201014)\\Result\\sunmoon-copy.jpg");
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            BufferedOutputStream bos = new BufferedOutputStream(fos, 20);
+
+            int c;
+            while((c = fis.read()) != -1){
+                fos.write((byte)c);
+            }
+            fis.close();
+            fos.close();
         }
-        fis.close();
-        fos.close();
+        catch(IOException e){
+            System.out.println("입출력 오류 발생!");
+        }
     }
 }
